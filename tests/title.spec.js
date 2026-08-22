a5934d9a099b9ff626bb3f58641783756679e1e1
Vijaya <vijaya.salveru@wnco.com>
2026-08-22 14:23:30 +0530

    new branch

--- tests/title.spec.js
+++ tests/title.spec.js
@@ -11,5 +11,22 @@ test('verify page url', async ({ page }) => {
   await expect(page).toHaveURL('https://practice.expandtesting.com/login');
 });
 
+test('successful login', async ({ page }) => {
+  await page.goto('https://practice.expandtesting.com/login');
+  await page.fill('#username', 'practice');
+  await page.fill('#password', 'SuperSecretPassword!');
+  await page.click('button[type="submit"]');
+  await expect(page).toHaveURL(/secure/);
+});
+
+test('failed login', async ({ page }) => {
+  await page.goto('https://practice.expandtesting.com/login');
+  await page.fill('#username', 'wronguser');
+  await page.fill('#password', 'wrongpass');
+  await page.click('button[type="submit"]');
+  await expect(page.locator('.flash.error')).toBeVisible();
+});
+
+
 
 
