34e5a8e6bb5945bd9eca7ea0593ef79fe1958ec5
Vijaya <vijaya.salveru@wnco.com>
2026-08-22 14:23:52 +0530

    new branch

--- playwright.config.js
+++ playwright.config.js
@@ -10,11 +10,22 @@ module.exports = defineConfig({
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
+    launchOptions: {
+      slowMo: 1000,
+    },
   },
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
     },
   ],
 });
